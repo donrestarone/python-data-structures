287fcb5677565bfba4af106b5dfac2e6a39e9026
donrestarone <shashikejayatunge@gmail.com>
2019-02-03 21:40:46 -0500

    intro to lists

---
+++ lists.py
@@ -0,0 +1,18 @@
+# import numpy library aliased as np
+import numpy as np
+
+# generate a normal distribution given 3 parameters
+a = np.random.normal(55, 5.0, 10)
+
+# print (a)
+
+listX = [1,2,3,4,5,6]
+listX3rdIndexAndUp = listX[3:]
+# print(listX3rdIndexAndUp)
+
+listXLast2Indexes = listX[-2:]
+print(listXLast2Indexes)
+
+listOfLists = [listX, listX3rdIndexAndUp, listXLast2Indexes]
+
+print(listOfLists)
